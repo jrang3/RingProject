67a56df0725ba51dfec8fde6ee733202ce57e236
jrang3 <jrang3@illinois.edu>
2023-07-25 15:14:02 -0500

    Continued to Debug
    Placed another conditional and added more debug statements to figure why the spheres won't move

--- Assets/Scripts/FormationController.cs
+++ Assets/Scripts/FormationController.cs
@@ -23,24 +23,29 @@ public class FormationController : MonoBehaviour
     {
         //Creates ring of circles
         Vector3 targetPosition = transform.position; //what we want the spheres to be circled around
-        for (int i = 0; i < 10; i++)
-        { //populate 10 spheres on screen
-            float angle = i * (2 * Mathf.PI / 10); //find an angle for every sphere that we want to populate, divide by 10 because that's number of spheres we want
+        Debug.Log(targetPosition);
+        Vector3 compare = new Vector3(9.72f, 6.45f, -11.39f);
+        if (targetPosition != compare)
+        {
+            for (int i = 0; i < 10; i++)
+            { //populate 10 spheres on screen
+                float angle = i * (2 * Mathf.PI / 10); //find an angle for every sphere that we want to populate, divide by 10 because that's number of spheres we want
 
-            //Users have ability to change radius of ring (offset for each sphere)
-            float x = Mathf.Cos(angle) * radius;
-            float y = Mathf.Sin(angle) * radius;
+                //Users have ability to change radius of ring (offset for each sphere)
+                float x = Mathf.Cos(angle) * radius;
+                float y = Mathf.Sin(angle) * radius;
 
-            targetPosition = new Vector3(targetPosition.x + x, targetPosition.y + y, 0);
-            Vector3 compare = new Vector3(9.72f, 6.45f, 0.00f);
-            if (targetPosition != compare)
-            { //For some reason an extra sphere at compare location is being instatiated (prevents that from happening)
-                GameObject instance = Instantiate(prefab);
-                gameObjects.Add(instance);
-                instance.transform.position = targetPosition;
-                points_.Add(targetPosition);
+                targetPosition = new Vector3(targetPosition.x + x, targetPosition.y + y, 0);
+                Vector3 compare_two = new Vector3(9.72f, 6.45f, 0.00f);
+                if (targetPosition != compare_two)
+                { //For some reason an extra sphere at compare location is being instatiated (prevents that from happening)
+                    GameObject instance = Instantiate(prefab);
+                    gameObjects.Add(instance);
+                    instance.transform.position = targetPosition;
+                    points_.Add(targetPosition);
+                }
+
             }
-
         }
     }
 
@@ -54,7 +59,7 @@ public class FormationController : MonoBehaviour
 
             //take difference between game object pos and the empty ring position and normalize the vector (converting that to unit vector)
 
-            Vector3 unitVector = (points_[i] - transform.position).normalized;
+            Vector3 unitVector = (points_[i] - transform.position).normalized; 
 
             float x_plus_output = unitVector.x * amplitude * Mathf.Cos(Time.time * speed * Mathf.Deg2Rad / Mathf.PI);
             float y_plus_output = unitVector.y * amplitude * Mathf.Cos(Time.time * speed * Mathf.Deg2Rad / Mathf.PI);
@@ -68,14 +73,7 @@ public class FormationController : MonoBehaviour
             gameObjects[i].transform.position = points_[i] + percent_plus / 100 * plus_output + percent_cross / 100 * cross_output;
 
 
-            //New Code (Dhruv's Implementation also doesn't seem to work very similar to what I did above
-            //Vector3 new_plus_output = MovePlusMode(points_[i], transform.position);
-            //Vector3 new_cross_ouput = MoveCrossMode(points_[i], transform.position);
-
-            // gameObjects[i].transform.position.x = points_[i].x + (percent_plus / 100 * new_plus_output.x + percent_cross / 100 * new_cross_ouput.x) / 100;
-            //gameObjects[i].transform.position = points_[i] + percent_plus / 100 * new_plus_output / 100 + percent_cross / 100 * new_cross_ouput / 1000;
-
-
+        
 
         }
 
